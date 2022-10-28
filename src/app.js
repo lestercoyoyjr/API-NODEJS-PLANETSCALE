9c3f1ad687c33b0ec499090a430d69a84ad8afcb
lestercoyoy <lester_cch@hotmail.com>
2022-10-28 11:38:11 -0600

    ADD
    DB connection to Planetscale

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import db from "./config/db.js";
+
 const app = express();
 
 app.set("port", process.env.PORT || 3000)
@@ -11,3 +13,11 @@ app.use("/", (req,res) => {
 app.listen(3000, () => {
     console.log("server is running on port ", app.get("port"));
 })
+
+db.connect()
+.then(()=>{
+    console.log("Connected to database");
+})
+.catch((err) =>{
+    console.log("Error! ", err);
+});

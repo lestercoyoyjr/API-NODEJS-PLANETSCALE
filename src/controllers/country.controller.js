f41737b34ea60db07d35f0264c3226d6492e2460
lestercoyoy <lester_cch@hotmail.com>
2022-11-04 16:31:47 -0600

    ADD
    Country controllers

---
+++ src/controllers/country.controller.js
@@ -0,0 +1,19 @@
+export const getCountries = (req,res) => {
+    res.send("countries");
+}
+
+export const getCountry = (req,res) => {
+    res.send("countries");
+}
+
+export const createCountry = (req,res) => {
+    res.send("countries");
+}
+
+export const updateCountry = (req,res) => {
+    res.send("countries");
+}
+
+export const deleteCountry = (req,res) => {
+    res.send("countries");
+}

62b86f35105c708ee784446f9412aef47955401a
lestercoyoy <lester_cch@hotmail.com>
2022-11-04 16:32:08 -0600

    MODIFY
    Country endpoints

--- src/routes/country.route.js
+++ src/routes/country.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
+import {getCountries, getCountry, createCountry, updateCountry, deleteCountry} from "../controllers/country.controller.js";
+
 const countryRouter = Router();
 
-countryRouter.get("/", (req,res) => {
-    res.send("countries");
-})
+countryRouter.get("/", getCountries); 
+
+countryRouter.get("/:id", getCountry);
+
+countryRouter.post("/", createCountry);
+
+countryRouter.put("/:id", updateCountry);
+
+countryRouter.delete("/:id", deleteCountry);
 
 export default countryRouter;
